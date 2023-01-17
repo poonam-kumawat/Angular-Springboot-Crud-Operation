51c61de1d91bf77c811fe95f5d4788ab4105264b
poonam-kumawat <mailpoonam2002@gmail.com>
2023-01-17 11:09:33 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JobListComponent } from './job-list/job-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'jobs',component:JobListComponent},
+  {path:"",redirectTo:'jobs',pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
